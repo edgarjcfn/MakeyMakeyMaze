b67cee078f17effe79c876912694601163287e71
Edgar Neto <edgar.figueiredo@gmail.com>
2013-03-18 08:40:42 +0100

    Removed Grid Framework.

---
+++ Assets/Scripts/PlayAgainButton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayAgainButton : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		var msg = GetComponent<UIButtonMessage>();
+		msg.target = gameObject;
+		msg.functionName = "OnPlayAgainClick";
+	}
+
+	void OnPlayAgainClick()
+	{
+		Application.LoadLevel("GameScene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
